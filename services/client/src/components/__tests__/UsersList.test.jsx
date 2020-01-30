5541d680a01f5e94861a1200724133196577c56f
Vilius Gudziunas <vilius.gudziunas@yahoo.com>
2020-01-30 21:41:21 +0000

    Part 2 - Testing React

---
+++ services/client/src/components/__tests__/UsersList.test.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
+import UsersList from '../UsersList';
+
+const users = [
+  { active: true, email: 'test@test.com', id: 1 },
+  { active: true, email: 'test2@test.com', id: 2 }
+];
+
+describe('UsersList', () => {
+  it('renders properly', () => {
+    const wrapper = shallow(<UsersList users={users} />);
+    const element = wrapper.find('.list-group-item');
+
+    expect(element.length).toBe(2);
+    expect(element.get(0).props.children).toBe('test@test.com');
+    expect(element.get(1).props.children).toBe('test2@test.com');
+  });
+
+  it('renders a snapshot properly', () => {
+    const tree = renderer.create(<UsersList users={users} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
