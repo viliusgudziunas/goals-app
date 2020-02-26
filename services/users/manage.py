28baeefc67294d5d6c731390ea3a7eb4465a8253
Vilius Gudziunas <vilius.gudziunas@yahoo.com>
2020-02-26 22:06:00 +0000

    WIP: Missed one file in previous commit

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,6 +1,6 @@
 from flask.cli import FlaskGroup
 from project import create_app, db
-from project.api.models import User
+from project.models import User
 
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
